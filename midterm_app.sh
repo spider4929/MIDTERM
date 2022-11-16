1c01e08adcfb1e384c77398b6db0f7716324fcd9
kjqb4929@gmail.com <kjqb4929@gmail.com>
2022-11-16 01:25:55 +0000

    Docker

---
+++ midterm_app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/static
+mkdir tempdir/templates
+
+cp midterm_app.py tempdir
+cp -r static/* tempdir/static/.
+cp -r templates/* tempdir/templates/.
+
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates" >> tempdir/Dockerfile
+echo "COPY midterm_app.py /home/myapp" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/midterm_app.py"
+
+cd tempdir
+docker build -t midtermapp .
+docker run -t -d -p 5050:5050 --name midtermrun midtermapp
+docker ps -a
